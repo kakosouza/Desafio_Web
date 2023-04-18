f3ae07533f582a4e8566d5938b75a05b476c0332
Carlos Souza <kakosouza@gmail.com>
2023-04-18 16:48:11 -0300

    Insert na tabela fornecedor

---
+++ desafio/src/main/java/com/desafioweb/desafio/repositories/FornecedorRepository.java
@@ -0,0 +1,9 @@
+package com.desafioweb.desafio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.desafioweb.desafio.entities.Fornecedor;
+
+public interface FornecedorRepository extends JpaRepository<Fornecedor, String> {
+
+}
