ed50d632680f28f519a7bc3cec37f4845b69b7fe
Carlos Souza <kakosouza@gmail.com>
2023-04-18 17:32:55 -0300

    implementacao findid

--- desafio/src/main/java/com/desafioweb/desafio/resources/FornecedorResource.java
+++ desafio/src/main/java/com/desafioweb/desafio/resources/FornecedorResource.java
@@ -1,19 +1,34 @@
 package com.desafioweb.desafio.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desafioweb.desafio.entities.Fornecedor;
+import com.desafioweb.desafio.services.FornecedorService;
 
 @RestController
 @RequestMapping(value = "/fornecedores")
 public class FornecedorResource {
+	
+	@Autowired
+	private FornecedorService service;
 
 	@GetMapping
-	public ResponseEntity<Fornecedor> findAll() {
-		Fornecedor f = new Fornecedor("00011111111111", null, null, null, null, null, null, null, null, null, null, null);
-		return ResponseEntity.ok().body(f);
+	public ResponseEntity<List<Fornecedor>> findAll() {
+		List<Fornecedor> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{chave}")
+	public ResponseEntity<Fornecedor> findById(@PathVariable String chave) {
+		Fornecedor forn = service.FindById(chave);
+		return ResponseEntity.ok().body(forn);
+	}
+	
 }
---
+++ desafio/src/main/java/com/desafioweb/desafio/services/FornecedorService.java
@@ -0,0 +1,26 @@
+package com.desafioweb.desafio.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.desafioweb.desafio.entities.Fornecedor;
+import com.desafioweb.desafio.repositories.FornecedorRepository;
+
+@Service
+public class FornecedorService {
+
+	@Autowired
+	private FornecedorRepository repository;
+	
+	public List<Fornecedor> findAll() {
+		return repository.findAll();
+	}
+	
+	public Fornecedor FindById(String chave) {
+		Optional<Fornecedor> obj = repository.findById(chave);
+		return obj.get();
+	}
+}
