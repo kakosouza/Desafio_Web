9f5b7dd107ae239f83faa8078b0f19d379086fdd
Carlos Souza <kakosouza@gmail.com>
2023-04-24 17:04:31 -0300

    Inclusão das rotas e paletas de cores

--- crud-angular/src/app/app-routing.module.ts
+++ crud-angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'cadastros' },
+  { path: 'cadastros',
+    loadChildren: () => import('./cadastros/cadastros.module').then(m => m.CadastrosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ crud-angular/src/app/cadastros/cadastros-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CadastrosComponent } from './cadastros/cadastros.component';
+
+const routes: Routes = [
+  { path: '', component: CadastrosComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CadastrosRoutingModule { }
