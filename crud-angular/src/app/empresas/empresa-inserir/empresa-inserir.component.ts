5cfb2466ae431299e79656e21eb4d21d57d29e57
Carlos Souza <kakosouza@gmail.com>
2023-05-11 18:13:56 -0300

    Inclusão da pesquisa do CEP

--- crud-angular/src/app/empresas/empresa-inserir/empresa-inserir.component.ts
+++ crud-angular/src/app/empresas/empresa-inserir/empresa-inserir.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 import { EmpresasService } from '../services/empresas.service';
 import { Location } from '@angular/common';
 import { Empresa } from 'src/shared/models/empresa.model';
+import { ConsultaCepService } from 'src/shared/services/consulta-cep.service';
 
 @Component({
   selector: 'app-empresa-inserir',
@@ -17,7 +18,8 @@ export class EmpresaInserirComponent implements OnInit {
   form: FormGroup;
   public cepInput: string = '';
   public dados = new XMLHttpRequest();
-//  private cepOk: string = '';
+  private retorno: string = '';
+  private resultado: string [] = [];
   private empresa: Empresa = this.route.snapshot.data['empresa'];
 
   constructor(private formBuilder: FormBuilder,
@@ -25,7 +27,8 @@ export class EmpresaInserirComponent implements OnInit {
       private snackBar: MatSnackBar,
       private location: Location,
       private route: ActivatedRoute,
-      private router: Router
+      private cepService: ConsultaCepService,
+      private rotaAtiva: ActivatedRoute
       ) {
       this.form = this.formBuilder.group({
         id: [0],
@@ -46,6 +49,8 @@ export class EmpresaInserirComponent implements OnInit {
 
   onSubmit() {
     if (this.form.valid !== null) {
+      console.log(this.form.value);
+      this.form.get('http://localhost:4200/empresas/new');
       this.service.save(this.form.value).subscribe(data => this.onSuccess());
     }
     else  {
@@ -59,8 +64,27 @@ export class EmpresaInserirComponent implements OnInit {
   }
 
   onCep() {
-    this.service.buscar(this.cepInput, this.dados);
+      if (this.cepInput != null && this.cepInput !== ''){
+        this.cepService.buscarCep(this.cepInput)
+        ?.subscribe(dados => this.populaDadosForm(dados));
+      }
   }
+
+  populaDadosForm(dados:any) {
+    this.form.patchValue({
+      cnpj: this.form.value.cnpj,
+      nome: this.form.value.nome,
+      logradouro: dados.logradouro,
+      numero: this.form.value.numero,
+      complemento: this.form.value.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf,
+      cep: dados.cep
+  })
+
+}
+
 
   onSuccess() {
     this.snackBar.open('Empresa salva com sucesso!', '', {duration: 5000});
---
+++ crud-angular/src/shared/services/consulta-cep.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConsultaCepService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+
+  buscarCep(cep: string) {
+        //Verifica se só tem dígitos
+        cep = cep.replace(/\D/g, '');
+
+        if (cep !== '') {
+           //Expressão regular para validar CEP
+           const validacep = /^[0-9]{8}$/;
+
+           //Valida formato do CEP
+           if (validacep.test(cep)) {
+              return this.http.get(`//viacep.com.br/ws/${cep}/json`);
+           }
+           alert(cep);
+        }
+        alert('Vazio');
+        return of({});
+  }
+}
