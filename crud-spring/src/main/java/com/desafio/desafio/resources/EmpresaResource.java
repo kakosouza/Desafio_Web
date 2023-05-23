e22be59c7f9d432522cbd1cb92e24c7effbdc21e
Carlos Souza <kakosouza@gmail.com>
2023-05-23 15:19:56 -0300

    Inclusão do Insert

--- crud-spring/src/main/java/com/desafio/desafio/resources/EmpresaResource.java
+++ crud-spring/src/main/java/com/desafio/desafio/resources/EmpresaResource.java
@@ -1,13 +1,17 @@
 package com.desafio.desafio.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.desafio.desafio.entities.Empresa;
 import com.desafio.desafio.services.EmpresaService;
@@ -29,5 +33,13 @@ public class EmpresaResource {
     public ResponseEntity<Empresa> findById(@PathVariable String cnpj) {
         Empresa obj = service.findById(cnpj);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Empresa> insert(@RequestBody Empresa obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{cnpj}").buildAndExpand(obj.getCnpj()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
